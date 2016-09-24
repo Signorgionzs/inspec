d43015cde8e2458080b0e561e04161c0a29e9fb1
Signorgionzs <John94antony@gmail.com>
2016-09-24 16:23:28 +0200

    Update ssl_poodle_test.rb

--- profile/controls/ssl_poodle_test.rb
+++ profile/controls/ssl_poodle_test.rb
@@ -1,4 +1,4 @@
-control 'poodle-1.0' do
+control 'CVE-2014-3566 poodle-1.0' do
   impact 4.3
   title 'check version ssl'
   desc 'The POODLE attack takes advantage of the protocol version negotiation feature built into SSL/TLS to force the use of SSL 3.0 and then leverages this new vulnerability to decrypt select content within the SSL session. The decryption is done byte by byte and will generate a large number of connections between the client and server.'
