4175e3ad8b63a19761a01a3e24627262ace7dac2
Signorgionzs <John94antony@gmail.com>
2016-10-10 12:41:12 +0200

    Delete jar.rb

--- profile/controls/jar.rb
+++
@@ -1,9 +0,0 @@
-control 'jar's existence' do
-  impact 0
-  title 'control the existence of the jar file'
-  tag 'gordon'
-  ref 
- describe file(/home/centos/ropeytasks.jar') do
- it { should exist }
-end
-end
